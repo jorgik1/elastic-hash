07499c111ad4b3b639799d9d3aca061baae27428
Yurii Stenin <yurii.stenin@dropsolid.com>
2025-03-02 18:40:23 +0100

    Update name of package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ Setup script for backward compatibility with older pip versions.
 from setuptools import setup, find_packages
 
 setup(
-    name="elastic-hash",
+    name="open-elastic-hash",
     version="0.1.0",
     packages=find_packages(),
     python_requires=">=3.7",
